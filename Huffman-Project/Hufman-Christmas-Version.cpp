b73dc27965cb9ca43e97125890ee44bb892f46b6
GeorgiMinkov <georgi_min@outlook.com>
2017-01-22 08:40:12 +0200

    Upload

---
+++ Huffman-Project/Hufman-Christmas-Version.cpp
@@ -0,0 +1,13 @@
+// Hufman-Christmas-Version.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Algorythm.h"
+
+int main()
+{
+	// lego("This is text.");
+	mainProgram();
+    return 0;
+}
+
